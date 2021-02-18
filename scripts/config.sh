e63daa8b5d98eae7149b98dfc2f5ee0704fa121a
rsosemann <robert@soesemann.com>
2021-02-18 15:20:25 +0100

    Missing changes from Apex labs merge

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 DEV_HUB_ALIAS="DevHubCopado"
 SCRATCH_ORG_ALIAS="salesforce-receipes_DEV"
+PACKAGE_NAME=""
+QA_ORG_ALIAS=""
--- scripts/createPackage.sh
+++ scripts/createPackage.sh
@@ -5,15 +5,27 @@ execute() {
   $@ || exit
 }
 
-if [ "$QA_URL" ]; then
-  echo "Authenticate QA ORG"
-  
-  echo $QA_URL > qaURLFile
-  sfdx force:auth:sfdxurl:store -f qaURLFile -a $QA_ORG_ALIAS
-  rm qaURLFile
+if [ -z "$secrets.DEV_HUB_URL" ]; then
+  echo "set default devhub user"
+  execute sfdx force:config:set defaultdevhubusername=$DEV_HUB_ALIAS
 fi
 
-PACKAGE_VERSION="$(execute sfdx force:package:version:create --package $PACKAGENAME --installationkeybypass --wait 10 --json | jq '.result.SubscriberPackageVersionId' | tr -d '"')"
-echo $PACKAGE_VERSION
+echo "List existing package versions"
+sfdx force:package:version:list -p $PACKAGE_NAME --concise
 
-execute sfdx force:package:install -p $PACKAGE_VERSION -u $QA_ORG_ALIAS --publishwait=3 --wait 10 -r
+echo "Create new package version"
+PACKAGE_VERSION="$(execute sfdx force:package:version:create -p $PACKAGE_NAME -x -w 10 --codecoverage --json | jq '.result.SubscriberPackageVersionId' | tr -d '"')"
+echo "Promote with: sfdx force:package:version:promote -p $PACKAGE_VERSION"
+echo "Install from: /packaging/installPackage.apexp?p0=$PACKAGE_VERSION"
+
+if [ $QA_ORG_ALIAS ]; then
+  if [ $secrets.QA_URL ]; then
+    echo "Authenticate QA Org"
+    echo $secrets.QA_URL > qaURLFile
+    execute sfdx force:auth:sfdxurl:store -f qaURLFile -a $QA_ORG_ALIAS
+    rm qaURLFile
+  fi
+
+  echo "Install in QA Org"
+  execute sfdx force:package:install -p $PACKAGE_VERSION -u $QA_ORG_ALIAS -b 10 -w 10 -r
+fi
